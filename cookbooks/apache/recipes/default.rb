f9593fd8494e1925afbc9ab4e3c30916cee72a88
Lindy Lewis <lindyslewis@gmail.com>
2018-03-05 14:24:09 +0000

    Added apache configuration to default.rb and template

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,6 +1,17 @@
-file '/home/vagrant/index.html' do
-  content '<html>From Mississippi with love.</html>'
+#Cookbook: apache
+#Recipe: default
+
+
+package 'httpd'
+
+template '/var/www/html/index.html' do 
   mode '0755'
-  owner 'vagrant'
-  group 'vagrant'
+  owner 'apache'
+  group 'apache'
+  source 'index.html.erb'
+end
+
+service 'httpd' do 
+  action [:enable, :start]
+  retries 3
 end
